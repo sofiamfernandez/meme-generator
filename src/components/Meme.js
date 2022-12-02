2157a40d5f1b08ff47c64ca0544f691b99ec7a6c
Sofia Fernandez <sofia.m.fernandez36@gmail.com>
2022-12-02 16:39:10 +0100

    color change

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,18 +1,26 @@
+
 import html2canvas from 'html2canvas';
+
+
 
 import React, { useState} from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Figure from 'react-bootstrap/Figure';
+import { InputGroup } from 'react-bootstrap';
+import { Badge } from 'react-bootstrap';
+
 
 
 import "./Meme.css"
+
 
 
 export const Meme = () => {
 //Estado inicial del editor de meme
 const [meme, setMeme] = useState(25);
 const [text, setText] = useState();
+const [color, setColor] =useState('#fff');
 
 const selectMeme = (e) => {
     setMeme(e.target.value);
@@ -21,8 +29,7 @@ const selectMeme = (e) => {
 
 const textMeme = (e) => {
     setText(e.target.value);
-    console.log(e.target.value);
-}
+  }
 
 const Download = (e) => {
     html2canvas(document.querySelector("#exportar")).then(function(canvas) {
@@ -44,7 +51,22 @@ const Download = (e) => {
             
             <Form.Label className='mt-3 mb-3 text-center'>Ingrese el texto del meme</Form.Label>
             <Form.Control onChange={textMeme} className='form-control w-50 m-50 m-auto d-block' type="text" placeholder="Pone tu frase" name="meme" arial-label="default input example" ></Form.Control>
-        
+            <Form.Label className='mt-3 mb-3 text-center'>Elegí el color de tu frase</Form.Label>
+            <InputGroup className="mb-3 input-group w-50 m-50 m-auto d-block">
+                <Badge bg='transparent'>
+                    <Form.Control
+                    name="color"
+                    type="color"
+                    aria-label="color"
+                    value={color} 
+                    onChange={(e) => setColor(e.target.value)}
+                    
+                    ></Form.Control>
+                </Badge>  
+            </InputGroup> 
+           
+
+
             <Form.Label className='mt-3 mb-3 text-center'>Elegí la imagen de tu meme</Form.Label>
             <Form.Select onChange={selectMeme} className='form-control form-select form-select-lg mb-3 w-50 m-auto' arial-label="form-select-lg example" >
                 <option value={0}>Seleccionar imagen</option>
@@ -52,11 +74,26 @@ const Download = (e) => {
                 <option value={2}>Futurama</option>
                 <option value={3}>Bob Esponja</option>
                 <option value={4}>Nena picarona</option>
+                <option value={5}>Gato vegetales</option>
+                <option value={6}>Cálculos</option>
+                <option value={7}>Gato desobediente</option>
+                <option value={8}>Homero</option>
+                <option value={9}>Mujer</option>
+                <option value={10}>Bon Esponja caja</option>
+                <option value={11}>Derrapando</option>
+                <option value={12}>Yo</option>
+                <option value={13}>Dice mi mamá</option>
+                <option value={14}>Payaso IT</option>
+                <option value={15}>Toy Story</option>
             </Form.Select>   
         
             <Figure className="text-center w-100"  id="exportar">
                 <Figure.Caption >
-                     <p className="texto-meme w-100 px-30 position-absolute top-60 start-30  h1 text-center ">{text}</p> 
+                    <p 
+                    className="w-100 px-30 position-absolute top-60 start-30  h1 text-center" 
+                    id="texto-meme"
+                    style={{color:color}}
+                    >{text}</p> 
                 </Figure.Caption>
 
                 <Figure.Image 
---
+++ src/components/Picker.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ChromePicker } from 'react-color';
+
+export const Picker = () => {
+  return (
+    <div>Picker</div>
+  )
+}
